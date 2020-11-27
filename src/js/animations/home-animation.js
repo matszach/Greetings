77a2351ea75a0f18fc236574f4c6a7cd7da09f99
matszach <lkaszubowski@gmail.com>
2020-11-27 21:35:15 +0100

    mobile animation tweak

--- src/js/animations/home-animation.js
+++ src/js/animations/home-animation.js
@@ -7,12 +7,14 @@ class HomeAnimation extends Animation {
         cw.clear();
         const f = loop.getFrame();
         const bRect = cw.getBoundingRect();
-        // 5 pixels takes care of the scrollbar caused irregularity
-        const center = bRect.getCenter().move(5, 0); 
+        const center = bRect.getCenter(); 
+        if(!isMobile) {
+            center.move(5, 0);
+        }
         const nofPentagons = Math.max(bRect.width, bRect.height)/70 + 10;
         Gmt.iter1D(nofPentagons, i => {
             const circle = center.toCircle(100 + 35 * i);
-            const dir = ((i / 120) + (f / 4000)) * 2 * Math.PI * (isMobile ? 2 : 1); 
+            const dir = ((i / 120) * (isMobile ? 2 : 1) + (f / 4000)) * 2 * Math.PI; 
             const pentagon = circle.toPolygon(5, dir);
             let cs = (((f / 5) % 20 + i) % 10) * 15;
             cs = cs < 75 ? cs : 150 - cs;
